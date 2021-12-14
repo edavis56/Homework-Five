fb34c50db67f9a0f5a20439fa57fc99986c57a26
edavis56 <92826817+edavis56@users.noreply.github.com>
2021-12-13 21:42:34 -0500

    Update to script.js, setting up variables

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,19 @@
-const GeneratorForm = document.querySelector("#passwordGenerator")
-const containerDiv = document.querySelector("#container")
-const resultsDiv = document.querySelector("#resultContainer")
-const options = document.querySelector("#options")
-const optionsDiv = document.querySelector("#optionsContainer")
-const lowercase = document.querySelector("#lowercase")
-const uppercase = document.querySelector("#uppercase")
-const numeric = document.querySelector("#numeric")
-const special = document.querySelector("#special")
-const genButton = document.querySelector("#generateButton")
-const thePwd = document.querySelector("#thePwd")
+var eight = $("#8AM");
+var nine = $("#9AM");
+var ten = $("#10AM");
+var eleven = $("#11AM");
+var noon = $("#12PM");
+var one = $("#1PM");
+var two = $("#2PM");
+var three = $("#3PM");
+var four = $("#4PM");
+var five = $("#5PM");
+var six = $("#6PM");
 
-var characterLength = document.getElementById("length")
-var lower = 'abcdefghijklmnopqrstuv';
-var upper = 'ABCDEFGHIJKLMNOPQRSTUV';
-var num = '123456789';
-var symbol = '!@#$%^&*()_-';
+var Input;
 
-genButton.addEventListener("click",function(e){
-    var character = lower;
-    (lowercase.checked) ? character += lower : '';
-    (uppercase.checked) ? character += upper : '';
-    (numeric.checked) ? character += num : '';
-    (special.checked) ? character += symbol : '';
-    thePwd.value = password(characterLength.value, character);
-});
 
-function password(l,character){
-    var pwd = '';
-    for(var i=0; i<l; i++){
-        pwd += character.charAt(Math.floor(Math.random() * character.length));
-    }
-    return pwd;
+function init() {
+    let initNine = JSON.parse(localStorage.getItem("08:00 AM"));
+    eight.val(initNine);
 }
-
-
