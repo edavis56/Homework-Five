b1991e3e33eb55ef958001ed5333ef30a4648c88
edavis56 <92826817+edavis56@users.noreply.github.com>
2021-12-13 21:49:14 -0500

    update to css link in html

--- assets/script.js
+++ assets/script.js
@@ -12,8 +12,42 @@ var six = $("#6PM");
 
 var Input;
 
-
 function init() {
     let initNine = JSON.parse(localStorage.getItem("08:00 AM"));
     eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("09:00 AM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("10:00 AM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("11:00 AM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("12:00 PM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("01:00 PM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("02:00 PM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("03:00 PM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("04:00 PM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("05:00 PM"));
+    eight.val(initNine);
+
+    let initNine = JSON.parse(localStorage.getItem("06:00 PM"));
+    eight.val(initNine);
+}
+
+$(".saveBtn").on("click", function(){
+   Input = $(this).siblings(".") 
+   console.log(Input);
 }
